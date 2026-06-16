2452d316202522f19d07c1696aa09381a1e2c16f
Danilo Pagliochi Araujo <danilo62195026@alunos.sc.senac.br>
2026-06-15 21:20:49 -0300

    Conferencia de logica basica

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,6 +2,10 @@ package helloworld;
 
 public class HelloWorld {
     public static void main(String[]args){
-        if(1==1){System.out.print("Alo??");}
+        if(1==1){
+            System.out.print("Alo??");
+        }else{
+            System.out.print("[ERROR MENSAGE]: logic error computation");
+        }
     }
 }
